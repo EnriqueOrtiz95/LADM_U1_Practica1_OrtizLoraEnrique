e5bdd7b39f825faaf2e05d33f3a21658ed41737f
Enrique Ortiz <enriquechivas_@hotmail.com>
2020-02-07 19:22:26 -0700

    Practica 1 Aplicaciones Moviles subida

---
+++ app/src/main/java/mx/edu/ladm_u1_practica_ortizloraenrique/Main4Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ladm_u1_practica_ortizloraenrique
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
---
+++ app/src/main/java/mx/edu/ladm_u1_practica_ortizloraenrique/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ladm_u1_practica_ortizloraenrique
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
